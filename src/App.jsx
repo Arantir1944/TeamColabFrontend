c12f75688140fbf30ccd581107d622666a975ddb
Arantir1944 <bogdan1necula@yahoo.com>
2025-02-24 18:14:08 +0200

    added protected route to register

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
+      <Route path="/register" element={<ProtectedRoute><Register /></ProtectedRoute>} />
       <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
       <Route path="/kanban" element={<ProtectedRoute><Kanban /></ProtectedRoute>} />
       <Route path="*" element={<Navigate to="/login" />} />
