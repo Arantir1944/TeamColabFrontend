5e5a3c3013ee267e491bcfc74b51e3f656772b58
Arantir1944 <bogdan1necula@yahoo.com>
2025-05-17 11:38:20 +0300

    fixed socket.io

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 import { getToken } from "../services/authService";  // :contentReference[oaicite:0]{index=0}&#8203;:contentReference[oaicite:1]{index=1}
 
 const token = getToken()?.split(" ")[1]; // strip "Bearer "
-export const socket = io("http://localhost:5000", {
+export const socket = io("https://16.170.210.30:5001", {
     auth: { token }
 });
 
